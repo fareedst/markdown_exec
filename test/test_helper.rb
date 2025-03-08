09e5edaa6b9d739a5ea4935bf36f6062f0d720ed
Fareed Steveson <fareedst>
2025-03-08 09:36:06 -0800

    All minitests are run in verbose mode by default

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,10 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'markdown_exec'
 
 require 'minitest/autorun'
+
+module Minitest
+  # ensures all tests run in verbose mode by default
+  def self.plugin_verbose_init(options)
+    self.reporter << Minitest::Reporter.new(options[:io], true)
+  end
+end
